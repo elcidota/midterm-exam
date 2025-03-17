10429861b665921f1fd2f6e9d92ade65d537ecdd
LAPTOP-GFSOT4B7\ACER <contact.elcid@gmail.com>
2025-03-17 14:32:18 +0800

    Midterm Exam

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TableController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/EspeletaDon', 'App\Http\Controllers\TableController@index');
